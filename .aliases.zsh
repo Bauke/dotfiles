19a1d476e661db0758fc5260678fc801599d9f0a
Bauke <me@bauke.xyz>
2022-05-11 22:54:31 +0200

    Add ZSH configuration and aliases.

---
+++ .aliases.zsh
@@ -0,0 +1,14 @@
+alias o="xdg-open"
+alias trash="gio trash"
+alias youtube-dl="yt-dlp"
+
+alias dotfiles="git --git-dir $HOME/.dotfiles --work-tree $HOME"
+alias gap="git add --patch"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias gl="git log"
+alias gls="git log --all --decorate --oneline --graph"
+alias gs="git status"
+
+alias clipboard-to-file="xclip -sel clip -o > $1"
+alias file-to-clipboard="xclip -sel clip -i $1"
---
+++ .zshrc
@@ -0,0 +1,15 @@
+source "$HOME/.oh-my-zsh/oh-my-zsh.sh"
+
+export EDITOR=nano
+export LESS="-F -X $LESS"
+
+export CARGO_BIN="$HOME/.cargo/bin"
+export LOCAL_BIN="$HOME/.local/bin"
+
+export PATH="$PATH:$CARGO_BIN"
+export PATH="$PATH:$LOCAL_BIN"
+
+source "$HOME/.zsh_aliases"
+
+eval "$(register-python-argcomplete pipx)"
+eval "$(starship init zsh)"
