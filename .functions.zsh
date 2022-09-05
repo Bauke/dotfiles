9ac06f1421622651a38a5d8bbdbcbdfb80c2968b
Bauke <me@bauke.xyz>
2022-09-05 14:34:28 +0200

    Add get-feed-url.

--- .functions.zsh
+++ .functions.zsh
@@ -1,3 +1,12 @@
 gtag () {
   git tag -s -a "$1" -m "Version $1"
 }
+
+get-feed-url () {
+  for input_url in "$@"; do
+    html=$(curl -fsLS "$input_url")
+    echo $(echo $html | select-html '[property="og:title"]' -a "content")
+    echo $(echo $html | select-html '[rel="alternate"][type]' -a "href")
+    echo
+  done
+}
