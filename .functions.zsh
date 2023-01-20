ea44a161a47679cfb87c3903b569fefee703ecad
Bauke <me@bauke.xyz>
2023-01-20 11:09:01 +0100

    Add descriptions to functions.

--- .functions.zsh
+++ .functions.zsh
@@ -1,11 +1,17 @@
+# Copies a section from a video using ffmpeg.
+# extract-clip <input file> <start timestamp> <end timestamp> <output file>
 extract-clip () {
   ffmpeg -i "$1" -ss "$2" -to "$3" -c copy "$4"
 }
 
+# Creates a new signed, annotated git tag.
+# gtag <version number>
 gtag () {
   git tag -s -a "$1" -m "Version $1"
 }
 
+# Tries to find a feed URL for any given URLs.
+# get-feed-url <url ...>
 get-feed-url () {
   for input_url in "$@"; do
     html=$(curl -fsLS "$input_url")
@@ -15,6 +21,8 @@ get-feed-url () {
   done
 }
 
+# Converts any given files to 128K Opus using ffmpeg.
+# to-opus <file ...>
 to-opus () {
   for input_file in "$@"; do
     output_file="${input_file%.*}.opus"
