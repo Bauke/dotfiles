f6f6294b28489d975c1cecb0640df2e57d6b34be
Bauke <me@bauke.xyz>
2023-02-05 11:55:24 +0100

    Add comments to ZSH files.

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,3 +1,4 @@
+# General aliases.
 alias c.="codium ."
 alias cat="bat"
 alias dust="dust -bs"
@@ -9,6 +10,7 @@ alias tar-extract="tar -x -f"
 alias tar-list="tar -t -f"
 alias trash="gio trash"
 
+# Git aliases.
 alias dotfiles="GIT_DIR=$HOME/.dotfiles GIT_WORK_TREE=$HOME"
 alias gap="git add --patch"
 alias gd="git diff"
@@ -20,8 +22,10 @@ alias gr="git remote"
 alias grv="git remote -v"
 alias gs="git status"
 
+# Clipboard aliases.
 alias clipboard-to-file="xclip -sel clip -o > $1"
 alias file-to-clipboard="xclip -sel clip -i $1"
 alias gpg-decrypt-clipboard="xclip -sel clip -o | gpg --decrypt"
 
+# See '$BAUKE_DIR/Restic Backups.md' for information.
 alias restic-b2="source $BAUKE_DIR/data/restic-b2-credentials.zsh && restic"
--- .zle.zsh
+++ .zle.zsh
@@ -1,2 +1,5 @@
+# See "man zshzle" for ZLE documentation.
+
+# Paste URLs inside quotes if there are special characters.
 autoload -Uz bracketed-paste-url-magic
 zle -N bracketed-paste bracketed-paste-url-magic
