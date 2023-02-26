d68d397f660cc7e598c2c27f0b857d48082b1eee
Bauke <me@bauke.xyz>
2023-02-26 14:03:53 +0100

    Add the dependencies file.

---
+++ .bauke/scripts/dependencies.ts
@@ -0,0 +1,6 @@
+export { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+export * as prompt from "https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts";
+
+export * as nodeFs from "https://deno.land/std@0.167.0/node/fs.ts";
+export * as nodeUtil from "https://deno.land/std@0.167.0/node/util.ts";
+export * as toml from "https://deno.land/std@0.167.0/encoding/toml.ts";
