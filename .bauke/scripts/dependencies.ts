d7da74267bb6b1df1884da4812a7831468be98ff
Bauke <me@bauke.xyz>
2023-08-06 19:32:44 +0200

    Documentation!

--- .bauke/scripts/dependencies.ts
+++ .bauke/scripts/dependencies.ts
@@ -1,4 +1,8 @@
+// All dependency imports, exported in a single file.
+
+// Cliffy
 export { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 export * as prompt from "https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts";
 
+// Deno Standard Library
 export * as toml from "https://deno.land/std@0.190.0/toml/mod.ts";
