49c525728d810ddc704eccab22344511bd9f60a2
Bauke <me@bauke.xyz>
2023-08-21 17:44:19 +0200

    Begin Azedia machine configuration.

---
+++ .bauke/nix/azedia/configuration.nix
@@ -0,0 +1,30 @@
+# The main configuration file.
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix # Created by nixos-generate-config.
+  ];
+
+  nix = {
+    settings.experimental-features = [ "flakes" "nix-command" ];
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      packageOverrides = pkgs: {
+        # Add the unstable channel as a separate package set.
+        unstable = import <nixos-unstable> {
+          # Pass config through so everything is shared between all channels.
+          config = config.nixpkgs.config;
+        };
+      };
+    };
+  };
+
+  # Before changing this value read the documentation for this option!
+  # https://search.nixos.org/options?channel=22.11&show=system.stateVersion
+  system.stateVersion = "22.11";
+}
