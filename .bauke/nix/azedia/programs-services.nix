0dff532d2893114a88ef60d543df9a33ca439b7b
Bauke <me@bauke.xyz>
2023-10-17 14:42:05 +0200

    Disable password authentication and root login.

--- .bauke/nix/azedia/programs-services.nix
+++ .bauke/nix/azedia/programs-services.nix
@@ -18,6 +18,12 @@
       };
     };
 
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
   };
 }
